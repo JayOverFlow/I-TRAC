bfd91b068b6cdb569df5ff57cd187b4dde91a3b1
John Rex Duran <jayoverflow29@gmail.com>
2026-01-30 01:00:42 +0800

    Created the EmailVerificationController class to handle email verification logic

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\EmailVerificationService;
+use Illuminate\Http\Request;
+
+class EmailVerificationController extends Controller
+{
+    protected $emailVerificationService;
+    public function __construct(EmailVerificationService $emailVerificationService) {
+        $this->emailVerificationService = $emailVerificationService;
+    }
+
+    // Send the 6 digit verification code to the email
+    public function sendVerificationCode(Request $request) {
+        // Get the user email
+        $email = $request->email;
+
+        // Check rate limitng to avoid spam
+        if (!$this->emailVerificationService->canRequestNewCode($email)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please wait 5 minutes before requesting another code'
+                ],429 // This code means Too Many Requests
+            );
+        }
+
+        // If passed the guard block
+        // Create and send code
+        $code = $this->emailVerificationService->createVerificationCode($email);
+        // TODO: Send actual email later
+        return response()->json([
+            'success' => true,
+            'message' => '6-digit verification code sent successfully',
+            'code' => $code // Remove in production
+        ]);
+    }
+
+    // Verify the 6 digit code from the user if valid
+    public function verifyCode(Request $request) {
+        // Valdite the code
+        $isValid = $this->emailVerificationService->validateCode(
+            $request->email,
+            $request->code
+        );
+        
+        // If code is valid
+        if ($isValid) {
+            return response()->json([
+                'success' => true,
+                'message' => '6-digit code verified successfully',
+                ]
+            );
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Invalid or expired verification code',
+            ],
+            400 // This code means Bad Request
+        );
+    }
+    
+}
