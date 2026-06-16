d6d74bcb19d38afcbaf1ea490a41b50ec95a1277
emman <emmanferrer753@gmail.com>
2026-06-16 23:19:11 +0800

    feat: api controllers for scanning mr using mobile app

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\AccountSettingsController;
+use App\Http\Controllers\Api\MrApiController;
 
 Route::group(['prefix' => 'user'], function () {
     Route::post('login',        [AuthController::class, 'login']);
@@ -16,6 +17,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('profile/update', [AccountSettingsController::class, 'updateProfile'])->middleware('auth:sanctum');
     Route::post('password/update', [AccountSettingsController::class, 'updatePassword'])->middleware('auth:sanctum');
     Route::post('avatar/update', [AccountSettingsController::class, 'updateAvatar'])->middleware('auth:sanctum');
+    Route::post('mr/assign',    [MrApiController::class, 'assignItems'])->middleware('auth:sanctum');
+    Route::get('mr/items',     [MrApiController::class, 'getUserItems'])->middleware('auth:sanctum');
 });
 
 Route::get('departments', [DepartmentController::class, 'index']);
