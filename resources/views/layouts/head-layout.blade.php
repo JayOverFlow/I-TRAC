e066411f8447c8a2ed39fd3efc8d299f30d26849
John Rex Duran <jayoverflow29@gmail.com>
2026-06-22 12:05:14 +0800

    style: apply custom loader animation

--- resources/views/layouts/head-layout.blade.php
+++ resources/views/layouts/head-layout.blade.php
@@ -44,7 +44,7 @@
     <div id="load_screen">
         <div class="loader">
             <div class="loader-content">
-                <div class="spinner-grow align-self-center"></div>
+                <img src="{{ asset('img/custom-loader.webp') }}" class="custom-loader align-self-center" alt="Loading...">
             </div>
         </div>
     </div>
