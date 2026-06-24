0561f2f6af6e885350996226c401aefa29e6dc6a
John Brix <coronejojohnbrix16@gmail.com>
2026-06-25 00:53:29 +0800

    style: inayos yung sa notif mag aadjust pag nabasa na, footer updated

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,15 @@
 <div class="footer-wrapper">
     <div class="footer-content-left">
         <span class="footer-tagline">MAKE IT EASY WITH</span>
-        <div class="footer-logo-container">
+        <div class="footer-logo-container" style="margin-bottom: 20px;">
             <img src="{{ asset('img/itrac-header-logo-white.svg') }}" alt="I-TRAC Logo" class="footer-logo">
         </div>
-        <p class="footer-description">
+        <p class="footer-description" style="margin-bottom: 40px;">
             A Digital System for Item Status Tracking and QR-Code Enabled Property Assignment
         </p>
+        <div class="footer-logo-container" style="margin-bottom: 15px;">
+            <img src="{{ asset('img/logo-footer.svg') }}" alt="I-TRAC Logo" class="footer-logo" style="height: 30px; width: auto;">
+        </div>
         <div class="footer-divider"></div>
         <p class="footer-copyright">
             Copyright &copy; 2026 I-TRAC. All rights reserved.
